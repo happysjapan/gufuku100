830acb423c97d673a042044b95ae78eb0ffecd9b
Mika Hashimoto <mikahashimoto@Mika-no-iMac.local>
2016-07-01 15:20:30 +0900

    product and recruitment were added in function as custom post type

--- wp/wp-content/themes/happys/includes/single/local-menu.php
+++ wp/wp-content/themes/happys/includes/single/local-menu.php
@@ -3,8 +3,8 @@
 
     <ul class="nav nav-pills">
       <li class="nav-pills--item"><a class="nav-pills--item--link" href="#">呉服店情報</a></li>
-      <li class="nav-pills--item"><a class="nav-pills--item--link" href="">取り扱い商品</a></li>
-      <li class="nav-pills--item"><a class="nav-pills--item--link" href="#">求人情報</a></li>
+      <li class="nav-pills--item"><a class="nav-pills--item--link" href="<?php echo get_the_permalink(get_field('shop_product_page')->ID); ?>">取り扱い商品</a></li>
+      <li class="nav-pills--item"><a class="nav-pills--item--link" href="<?php echo get_the_permalink(get_field('recruitment_info_page')->ID); ?>">求人情報</a></li>
     </ul>
   </div>
 </div>
