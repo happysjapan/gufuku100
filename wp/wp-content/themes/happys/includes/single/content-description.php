072ba9b5681725f9ed26736f3954036da5ffd31b
Mika Hashimoto <mikahashimoto@Mika-no-iMac.local>
2016-07-05 15:25:28 +0900

    changed the menu

--- wp/wp-content/themes/happys/includes/single/content-description.php
+++ wp/wp-content/themes/happys/includes/single/content-description.php
@@ -8,49 +8,49 @@
           <tr>
             <th width="200">会社名</th>
             <td>
-              <?php echo post_custom("shop_company_name"); ?>
+              <?php echo get_field("shop_company_name"); ?>
             </td>
           </tr>
           <tr>
             <th>代表者名</th>
-            <td><?php echo post_custom("shop_member_name"); ?></td>
+            <td><?php the_field("shop_member_name"); ?></td>
           </tr>
           <tr>
             <th>事業紹介</th>
-            <td><?php echo post_custom("shop_description"); ?></td>
+            <td><?php the_field("shop_description"); ?></td>
           </tr>
           <tr>
             <th>設立</th>
-            <td><?php echo post_custom("shop_establish"); ?>年</td>
+            <td><?php the_field("shop_establish"); ?>年</td>
           </tr>
           <tr>
             <th>従業員数</th>
-            <td><?php echo post_custom("shop_member_number"); ?>名</td>
+            <td><?php the_field("shop_member_number"); ?>名</td>
           </tr>
           <tr>
             <th>所在地</th>
-            <td><?php echo post_custom("shop_address"); ?></td>
+            <td><?php the_field("shop_address"); ?></td>
           </tr>
           <tr>
             <th>定休日</th>
-            <td><?php echo post_custom("shop_date"); ?></td>
+            <td><?php the_field("shop_date"); ?></td>
           </tr>
           <tr class="success">
             <th>電話番号/FAX</th>
-            <td><?php echo post_custom("shop_tel"); ?></td>
+            <td><?php the_field("shop_tel"); ?></td>
           </tr>
           <tr>
             <th>メールアドレス</th>
-            <td><?php echo post_custom("shop_email"); ?></td>
+            <td><?php the_field("shop_email"); ?></td>
           </tr>
           <tr>
             <th>HP</th>
-            <td><a href="<?php the_field("shop_hp"); ?>" target="_blank"><?php echo post_custom("shop_hp"); ?></a></td>
+            <td><a href="<?php the_field("shop_hp"); ?>" target="_blank"><?php the_field("shop_hp"); ?></a></td>
           </tr>
         </table>
 
         <h3 id="stuff" class="block-detail--information--title">編集部からのコメント</h3>
-        <p>text<?php echo post_custom("editor_comment"); ?></p>
+        <p>text<?php the_field("editor_comment"); ?></p>
     </div>
   </div>
 </div>
--- wp/wp-content/themes/happys/includes/single/local-menu.php
+++ wp/wp-content/themes/happys/includes/single/local-menu.php
@@ -1,24 +1,20 @@
 
-<?php
-$queried_object = get_queried_object();
-
-echo "<pre>";
-var_dump(get_post_type());
-?>
-
 
 <?php if ('post' == get_post_type()) {
-  echo 'class = "active"';
-} ?>
-
-
-<?php if ('product' == get_post_type()) {
-  echo 'class = "active"';
-} ?>
-
-
-<?php if ('recruitment' == get_post_type()) {
-  echo 'class = "active"';
+  $link_shop = get_the_permalink();
+  $link_product = get_the_permalink(get_field('product_page')->ID);
+  $link_recruitment = get_the_permalink(get_field('recruitment_page')->ID);
+  $shop_active = 'active';
+} else if ('product' == get_post_type()) {
+  $link_shop = get_the_permalink(get_field('product_shop_page')->ID);
+  $link_product = get_the_permalink();
+  $link_recruitment = get_the_permalink(get_field('product_recruitment_page')->ID);
+  $product_active = 'active';
+} else if ('recruitment' == get_post_type()) {
+  $link_shop = get_the_permalink(get_field('recruitment_shop_page')->ID);
+  $link_product = get_the_permalink(get_field('recruitment_product_page')->ID);
+  $link_recruitment = get_the_permalink();
+  $recruitment_active = 'active';
 } ?>
 
 
@@ -28,9 +24,9 @@ var_dump(get_post_type());
   <div class="columns">
 
     <ul class="nav nav-pills button-group">
-      <li class="nav-pills--item"><a class="button nav-pills--button" href="<?php the_field(' post '); ?>">呉服店情報</a></li>
-      <li class="nav-pills--item"><a class="button nav-pills--button" href="<?php the_field(' product '); ?>">取り扱い商品</a></li>
-      <li class="nav-pills--item"><a class="button nav-pills--button" href="<?php the_field(' recruitment '); ?>">求人情報</a></li>
+      <li class="nav-pills--item"><a class="button nav-pills--button <?php echo $shop_active; ?>" href="<?php echo $link_shop; ?>">呉服店情報</a></li>
+      <li class="nav-pills--item"><a class="button nav-pills--button <?php echo $product_active; ?>" href="<?php echo $link_product; ?>">取り扱い商品</a></li>
+      <li class="nav-pills--item"><a class="button nav-pills--button <?php echo $recruitment_active; ?>" href="<?php echo $link_recruitment; ?>">求人情報</a></li>
     </ul>
   </div>
 </div>
