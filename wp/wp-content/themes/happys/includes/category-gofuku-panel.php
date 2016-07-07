2056bff1c4fe24c9d6331921ba4b203110ecf1c6
Mika Hashimoto <mikahashimoto@Mika-no-iMac.local>
2016-07-07 14:25:19 +0900

    panels and main color

--- wp/wp-content/themes/happys/includes/category-gofuku-panel.php
+++ wp/wp-content/themes/happys/includes/category-gofuku-panel.php
@@ -1,30 +1,39 @@
-<article id="post-<?php the_ID(); ?>" class="panel row">
-    <div class=" gofuku_panel columns">
+<article id="post-<?php the_ID(); ?>" class="row">
+  <div class="columns">
+    <div class="panel">
+
       <div class="panel--header row">
-
-          <h3 class="panel--title columns"><a href="<?php the_permalink(); ?>"  title="<?php the_title(); ?>" class="panel--title--link"><?php the_title(); ?></a></h3>
-
-      </div>
-
-
-
-      <div class="row">
-        <div class="medium-3 columns">
-            <img class="panel--thumbnail" src="<?php the_sub_field('medical_list_image')["sizes"]["thumbnail"]; ?>" alt="<?php the_title(); ?>" />
-
-
-            <!-- 代表名 -->
-          <p>代表名：<?php the_sub_field('medical_list_name'); ?></p>
-        </div>
-
-        <div class="medium-9 columns">
-          <h2 class="product_panel--title">100選ポイント</h2>
-           <?php echo get_field('shop_medical_list'); ?>
-           <a href="<?php the_permalink(); ?>"  title="詳しく見る" class="panel--articleLink">詳しく見る</a>
+        <div class="small-12 columns">
+          <h3 class="panel--title"><a href="<?php the_permalink(); ?>"  title="<?php the_title(); ?>" class="panel--title--link"><?php the_title(); ?></a></h3>
         </div>
       </div>
 
+      <div class="panel--main row">
+        <div class="small-12 columns">
+          <div class="row">
+            <div class="medium-3 columns">
+                <img class="panel--thumbnail" src="<?php echo the_field('clinic_top_image')["sizes"]["thumbnail"]; ?>" alt="<?php the_title(); ?>" />
 
+
+                <!-- 代表名 -->
+              <p>代表名：<?php echo get_field('clinic_representative'); ?></p>
+            </div>
+
+            <div class="medium-9 columns">
+               <h2 class="panel--main--title main_title">100選ポイント</h2>
+               <ul>
+                 <li><?php echo get_field('clinic_point1'); ?></li>
+                 <li><?php echo get_field('clinic_point2'); ?></li>
+                 <li><?php echo get_field('clinic_point3'); ?></li>
+               </ul>
+
+               <div class="button__panel--holder">
+                 <a href="<?php the_permalink(); ?>"  title="詳しく見る" class="button button__panel">詳しく見る</a>
+               </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-
+  </div>
 </article>
