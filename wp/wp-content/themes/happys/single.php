09567608514fbd63cccb4a1d4965e2e7df047490
julian-printemps <julian.printemps@gmail.com>
2016-07-08 17:35:50 +0900

    fix block content single page

--- wp/wp-content/themes/happys/single.php
+++ wp/wp-content/themes/happys/single.php
@@ -1,20 +1,17 @@
 <?php get_header(); ?>
   <div class="page_wrapper">
+    <?php while ( have_posts() ) : the_post(); ?>
 
-        <?php while ( have_posts() ) : the_post(); ?>
+      <h3 class="row columns"><?php the_title(); ?></h3>
 
-          <h3 class="row columns"><?php the_title(); ?></h3>
+      <?php get_template_part( 'includes/single/local', 'menu' ); ?>
+      <?php get_template_part( 'includes/single/content', 'header' ); ?>
+      <?php get_template_part( 'includes/single/content', 'gallary' ); ?>
+      <?php get_template_part( 'includes/single/content', 'description' ); ?>
 
-          <?php get_template_part( 'includes/single/local', 'menu' ); ?>
-          <?php get_template_part( 'includes/single/content', 'header' ); ?>
-          <?php get_template_part( 'includes/single/content', 'gallary' ); ?>
-          <?php get_template_part( 'includes/single/content', 'description' ); ?>
+    <?php endwhile; ?>
 
-        <?php endwhile; ?>
 
-  </div>
-
-  <div class="page_wrapper">
     <div class="row">
       <div class="columns">
         <?php custom_breadcrumbs(); ?>
