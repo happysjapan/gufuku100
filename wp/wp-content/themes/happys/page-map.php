ab835a1d7f49885b83bb9ee1ab7fe406c1dc422d
Mika Hashimoto <mikahashimoto@Mika-no-iMac.local>
2016-07-11 15:08:21 +0900

    linked map

--- wp/wp-content/themes/happys/page-map.php
+++ wp/wp-content/themes/happys/page-map.php
@@ -26,7 +26,7 @@ get_header(); ?>
 				<div class="japan_map--area--holder row">
 
 					<div class="columns small-6 medium-4 japan_map--area area_1">
-						<a href="#" class="japan_map--area--link" title="">
+						<a href="<?php echo clinicCategoryLink('hokkaido_tohoku'); ?>" class="japan_map--area--link" title="">
 							<h3 class="japan_map--area--title">
 							北海道・東北</h3>
 							<ul class="japan_map--area--list row">
@@ -57,74 +57,74 @@ get_header(); ?>
 					</div>
 
 					<div class="columns small-6 medium-4 japan_map--area area_3">
-						<a href="#" class="japan_map--area--link" title="">
+						<a href="<?php echo clinicCategoryLink('chubu'); ?>" class="japan_map--area--link" title="">
 							<h3 class="japan_map--area--title">中部</h3>
 							<ul class="japan_map--area--list row">
-								<li class="japan_map--area--listitem columns small-6"><a href="#">新潟</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">富山</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">石川</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">福井</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">静岡</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">山梨</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">愛知</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">長野</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">岐阜</a></li>
+								<li class="japan_map--area--listitem columns small-6">新潟</li>
+								<li class="japan_map--area--listitem columns small-6">富山</li>
+								<li class="japan_map--area--listitem columns small-6">石川</li>
+								<li class="japan_map--area--listitem columns small-6">福井</li>
+								<li class="japan_map--area--listitem columns small-6">静岡</li>
+								<li class="japan_map--area--listitem columns small-6">山梨</li>
+								<li class="japan_map--area--listitem columns small-6">愛知</li>
+								<li class="japan_map--area--listitem columns small-6">長野</li>
+								<li class="japan_map--area--listitem columns small-6">岐阜</li>
 							</ul>
 						</a>
 					</div>
 
 					<div class="columns small-6 medium-4 japan_map--area area_4">
-						<a href="#" class="japan_map--area--link" title="">
+						<a href="<?php echo clinicCategoryLink('kinki'); ?>" class="japan_map--area--link" title="">
 							<h3 class="japan_map--area--title">近畿</h3>
 							<ul class="japan_map--area--list row">
-								<li class="japan_map--area--listitem columns small-6"><a href="#">大阪</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">京都</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">兵庫</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">奈良</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">三重</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">滋賀</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">和歌山</a></li>
+								<li class="japan_map--area--listitem columns small-6">大阪</li>
+								<li class="japan_map--area--listitem columns small-6">京都</li>
+								<li class="japan_map--area--listitem columns small-6">兵庫</li>
+								<li class="japan_map--area--listitem columns small-6">奈良</li>
+								<li class="japan_map--area--listitem columns small-6">三重</li>
+								<li class="japan_map--area--listitem columns small-6">滋賀</li>
+								<li class="japan_map--area--listitem columns small-6">和歌山</li>
 							</ul>
 						</a>
 					</div>
 
 					<div class="columns small-6 medium-4 japan_map--area area_5">
-						<a href="#" class="japan_map--area--link" title="">
+						<a href="<?php echo clinicCategoryLink('chugoku'); ?>" class="japan_map--area--link" title="">
 							<h3 class="japan_map--area--title">中国</h3>
 							<ul class="japan_map--area--list row">
-								<li class="japan_map--area--listitem columns small-6"><a href="#">鳥取</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">島根</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">広島</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">岡山</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">山口</a></li>
+								<li class="japan_map--area--listitem columns small-6">鳥取</li>
+								<li class="japan_map--area--listitem columns small-6">島根</li>
+								<li class="japan_map--area--listitem columns small-6">広島</li>
+								<li class="japan_map--area--listitem columns small-6">岡山</li>
+								<li class="japan_map--area--listitem columns small-6">山口</li>
 							</ul>
 						</a>
 					</div>
 
 					<div class="columns small-6 medium-4 japan_map--area area_6">
-						<a href="#" class="japan_map--area--link" title="">
+						<a href="<?php echo clinicCategoryLink('shikoku'); ?>" class="japan_map--area--link" title="">
 							<h3 class="japan_map--area--title">四国</h3>
 							<ul class="japan_map--area--list row">
-								<li class="japan_map--area--listitem columns small-6"><a href="#">香川</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">徳島</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">愛知</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">高知</a></li>
+								<li class="japan_map--area--listitem columns small-6">香川</li>
+								<li class="japan_map--area--listitem columns small-6">徳島</li>
+								<li class="japan_map--area--listitem columns small-6">愛知</li>
+								<li class="japan_map--area--listitem columns small-6">高知</li>
 							</ul>
 						</a>
 					</div>
 
 					<div class="columns small-6 medium-4 japan_map--area area_7">
-						<a href="#" class="japan_map--area--link" title="">
+						<a href="<?php echo clinicCategoryLink('kyushu_okinawa'); ?>" class="japan_map--area--link" title="">
 							<h3 class="japan_map--area--title">九州・沖縄</h3>
 							<ul class="japan_map--area--list row">
-								<li class="japan_map--area--listitem columns small-6"><a href="#">長崎</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">佐賀</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">熊本</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">福岡</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">大分</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">宮崎</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">鹿児島</a></li>
-								<li class="japan_map--area--listitem columns small-6"><a href="#">沖縄</a></li>
+								<li class="japan_map--area--listitem columns small-6">長崎</li>
+								<li class="japan_map--area--listitem columns small-6">佐賀</li>
+								<li class="japan_map--area--listitem columns small-6">熊本</li>
+								<li class="japan_map--area--listitem columns small-6">福岡</li>
+								<li class="japan_map--area--listitem columns small-6">大分</li>
+								<li class="japan_map--area--listitem columns small-6">宮崎</li>
+								<li class="japan_map--area--listitem columns small-6">鹿児島</li>
+								<li class="japan_map--area--listitem columns small-6">沖縄</li>
 							</ul>
 						</a>
 					</div>
@@ -132,25 +132,25 @@ get_header(); ?>
 
 
 				<div class="japan_map--icon--holder">
-					<a class="japan_map--icon--link" href="">
+					<a class="japan_map--icon--link" href="<?php echo clinicCategoryLink('hokkaido_tohoku'); ?>">
 						<svg class="japan_map--icon icon-japan-1"><use xlink:href="#icon-japan-1"></use></svg>
 					</a>
 					<a class="japan_map--icon--link" href="<?php echo clinicCategoryLink('kanto'); ?>">
 						<svg class="japan_map--icon icon-japan-2"><use xlink:href="#icon-japan-2"></use></svg>
 					</a>
-					<a class="japan_map--icon--link" href="">
+					<a class="japan_map--icon--link" href="<?php echo clinicCategoryLink('chubu'); ?>">
 						<svg class="japan_map--icon icon-japan-3"><use xlink:href="#icon-japan-3"></use></svg>
 					</a>
-					<a class="japan_map--icon--link" href="">
+					<a class="japan_map--icon--link" href="<?php echo clinicCategoryLink('kinki'); ?>">
 						<svg class="japan_map--icon icon-japan-4"><use xlink:href="#icon-japan-4"></use></svg>
 					</a>
-					<a class="japan_map--icon--link" href="">
+					<a class="japan_map--icon--link" href="<?php echo clinicCategoryLink('chugoku'); ?>">
 						<svg class="japan_map--icon icon-japan-5"><use xlink:href="#icon-japan-5"></use></svg>
 					</a>
-					<a class="japan_map--icon--link" href="">
+					<a class="japan_map--icon--link" href="<?php echo clinicCategoryLink('shikoku'); ?>">
 						<svg class="japan_map--icon icon-japan-6"><use xlink:href="#icon-japan-6"></use></svg>
 					</a>
-					<a class="japan_map--icon--link" href="">
+					<a class="japan_map--icon--link" href="<?php echo clinicCategoryLink('kyushu_okinawa'); ?>">
 						<svg class="japan_map--icon icon-japan-7"><use xlink:href="#icon-japan-7"></use></svg>
 					</a>
 				</div>
