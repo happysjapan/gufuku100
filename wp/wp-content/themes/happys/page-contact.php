3c6841bd31954f9897da4c048c9897621ec43b2e
Mika Hashimoto <mikahashimoto@Mika-no-iMac.local>
2016-07-11 19:03:30 +0900

    contact form

---
+++ wp/wp-content/themes/happys/page-contact.php
@@ -0,0 +1,36 @@
+<?php
+/*
+  Template Name: Page Contact
+*/
+get_header(); ?>
+
+<div class="row">
+	<div class="columns">
+		<?php custom_breadcrumbs(); ?>
+	</div>
+</div>
+
+<!-- [ #content ] -->
+<div class="page_wrapper">
+
+	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+		<div class="row">
+			<div class="columns">
+				<h2 class="form_title"><?php the_title(); ?></h2>
+				<div class="row">
+					<div class="main_form columns small-8">
+						<?php the_content(); ?>
+					</div>
+				</div>
+		 	</div>
+		</div>
+	<?php endwhile; ?>
+
+	<div class="row">
+		<div class="columns">
+			<?php custom_breadcrumbs(); ?>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
